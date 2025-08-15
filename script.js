7df0c965671fc41e60237edc30ac0413dfb00497
Wasteoidz <Rolf91@live.com>
2025-08-15 11:45:08 +0200

    added visittkort

--- script.js
+++ script.js
@@ -12,3 +12,19 @@
         document.getElementById(elementId).style.backgroundColor = randomColor();
         
     }
+
+    function popUp() {
+      window.open(href="visittkort.html",
+                    'targetWindow',
+                    `toolbar=no,
+                     location=no,
+                     status=no,
+                     menubar=no,
+                     scrollbars=yes,
+                     resizable=yes,
+                     width=315px,
+                     height=165px
+                     top=400px
+                     left=300px` 
+                     );
+    }
